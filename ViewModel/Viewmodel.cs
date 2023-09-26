c5c0efb619d9499d9d1f684e5881d58c28fe028f
win_park <psk0812@github.com>
2023-09-26 18:08:34 +0900

    "엑셀 연결완료"

--- ViewModel/Viewmodel.cs
+++ ViewModel/Viewmodel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using static clientcheck.Model.Model;
 
 namespace clientcheck.ViewModel
@@ -16,19 +19,40 @@ namespace clientcheck.ViewModel
 
         public Viewmodel()
         {
-            ClientLists = new ObservableCollection <client>
-            {
-                new client { Name = "박민준", Age = 23, Phonenumb = "010-5496-5056" ,delete = false},
-                new client { Name = "박서준", Age = 28, Phonenumb = "010-1234-5678" ,delete = false},
-                new client { Name = "이홍비", Age = 30, Phonenumb = "010-9876-5432",delete = false },
-                new client { Name = "김예준", Age = 23, Phonenumb = "010-5496-5056",delete = false },
-                new client { Name = "이하운", Age = 28, Phonenumb = "010-1234-5678",delete = false },
-                new client { Name = "제갈공명", Age = 30, Phonenumb = "010-9876-5432" ,delete = false},
-                new client { Name = "송채원", Age = 23, Phonenumb = "010-4690-9633",delete = false },
-                new client { Name = "석준", Age = 28, Phonenumb = "010-9657-3248",delete = false },
-                new client { Name = "이건후", Age = 30, Phonenumb = "010-3793-4615",delete = false },
-                new client { Name = "이히힉", Age = 30, Phonenumb = "010-3793-4615" ,delete = false}
-            };
+            ClientLists = new ObservableCollection<client>(); 
+            LoadDataFromCSV();
         }
+        public void LoadDataFromCSV()
+        {
+            try
+            {
+                using (var sr = new StreamReader("D:/notdie/clientcheck/Model/data.csv", Encoding.Default)) // 인코딩을 UTF-8로 설정
+                using (var parser = new TextFieldParser(sr))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+
+                        client newClient = new client
+                        {
+                            Name = fields[0],
+                            Age = int.Parse(fields[1]),
+                            Phonenumb = fields[2],
+                            delete = false // 기본값으로 설정
+                        };
+
+                        ClientLists.Add(newClient);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("CSV 파일을 불러오는 동안 오류가 발생했습니다: " + ex.Message);
+            }
+        }
+
     }
 }
